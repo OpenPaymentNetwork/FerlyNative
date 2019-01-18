9468fdda2148db23744b26a2bc8be3b3eacdd6a0
bbwilkes <bbradleywilkes@gmail.com>
2019-01-18 11:56:13 -0700

    Add hidden debug info button

--- src/ferly/views/Settings/Settings.js
+++ src/ferly/views/Settings/Settings.js
@@ -2,13 +2,36 @@
 import Icon from 'react-native-vector-icons/FontAwesome'
 import PropTypes from 'prop-types'
 import React from 'react'
-import {Constants} from 'expo'
+import {Notifications, Permissions, Constants} from 'expo'
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native'
 
 export default class Settings extends React.Component {
   static navigationOptions = {
     title: 'Settings'
   };
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      expoToken: '',
+      showDebug: false
+    }
+  }
+
+  componentDidMount () {
+    this.getToken()
+  }
+
+  async getToken () {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    )
+
+    if (existingStatus === 'granted') {
+      let token = await Notifications.getExpoPushTokenAsync()
+      this.setState({expoToken: token})
+    }
+  }
 
   renderItem (title, description, onPress = null) {
     let icon
@@ -37,12 +60,29 @@ export default class Settings extends React.Component {
     )
   }
 
+  toggleDebug () {
+    const {showDebug} = this.state
+    this.setState({showDebug: !showDebug})
+  }
+
   render () {
     const {navigation} = this.props
     const devFlag = __DEV__ ? 'd' : 'p'
     const version = `${Constants.manifest.version}/${devFlag}`
-    return (
+
+    let debugInfo = (
       <View>
+        <Text style={{fontSize: 12, color: 'lightgray'}}>
+          did: {Constants.deviceId}
+        </Text>
+        <Text style={{fontSize: 12, color: 'lightgray'}}>
+          et: {this.state.expoToken}
+        </Text>
+      </View>
+    )
+
+    return (
+      <View style={{flex: 1, backgroundColor: 'white'}}>
         {
           this.renderItem(
             'Account Recovery',
@@ -51,6 +91,14 @@ export default class Settings extends React.Component {
           )
         }
         {this.renderItem('About', `Version ${version}`)}
+        <View style={{flexGrow: 1}} />
+        <View style={{height: 40, flexDirection: 'row'}}>
+          {this.state.showDebug ? debugInfo : null}
+          <View style={{flexGrow: 1}} />
+          <TouchableOpacity
+            style={{width: 40, backgroundColor: 'white'}}
+            onPress={() => this.toggleDebug()} />
+        </View>
       </View>
     )
   }
