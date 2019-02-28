07d6ca91a427870d0f222eb3bb6261fa95972412
bbwilkes <bbradleywilkes@gmail.com>
2019-02-28 10:55:16 -0700

    Update Give screen design

--- src/ferly/views/Give/Give.js
+++ src/ferly/views/Give/Give.js
@@ -9,7 +9,14 @@ import {apiExpire, apiRequire} from 'ferly/store/api'
 import {connect} from 'react-redux'
 import {createUrl, post} from 'ferly/utils/fetch'
 import {StackActions} from 'react-navigation'
-import {View, Text, Alert, TextInput} from 'react-native'
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native'
 
 export class Give extends React.Component {
   static navigationOptions = {
@@ -18,6 +25,7 @@ export class Give extends React.Component {
 
   constructor (props) {
     super(props)
+    this.maxMessageLength = 400
     this.state = {amount: 0, error: '', submitting: false, message: ''}
   }
 
@@ -29,6 +37,8 @@ export class Give extends React.Component {
     const {navigation, apiExpire} = this.props
     const params = navigation.state.params
     const {design, user} = params
+    const {title} = design
+    const {first_name: firstName, last_name: lastName} = user
     const {amount, message} = this.state
     const formatted = accounting.formatMoney(parseFloat(amount))
 
@@ -51,8 +61,9 @@ export class Give extends React.Component {
             actions: [StackActions.push({routeName: 'Home'})]
           })
           navigation.dispatch(resetAction)
-          const desc = `You gifted ${formatted} ${design.title} to ${user.first_name} ${user.last_name}.`
-          Alert.alert('Complete!', desc)
+          Alert.alert(
+            'Complete!',
+            `You gifted ${formatted} ${title} to ${firstName} ${lastName}.`)
         } else {
           const error = json.invalid['amounts.0'] || json.invalid['amount']
           this.setState({error: error, amount: 0, submitting: false})
@@ -67,7 +78,7 @@ export class Give extends React.Component {
   render () {
     const params = this.props.navigation.state.params
     const {design} = params
-    const {amount, submitting, error} = this.state
+    const {amount, submitting, error, message} = this.state
     const amounts = this.props.amounts || []
     const fieldValue = accounting.formatMoney(parseFloat(amount))
 
@@ -78,34 +89,49 @@ export class Give extends React.Component {
     const foundAmount = found ? found.amount : 0
     const formatted = accounting.formatMoney(parseFloat(foundAmount))
 
+    const counter = (
+      <Text style={{color: 'lightgray'}}>
+        {`${message.length}/${this.maxMessageLength}`}
+      </Text>
+    )
+
     return (
       <View style={{flex: 1, backgroundColor: 'white'}}>
         <View style={{flex: 1}}>
-          <View style={{paddingHorizontal: 20, height: 60, justifyContent: 'space-between', flexDirection: 'row', borderBottomWidth: 0.5, borderColor: 'black', alignItems: 'center'}}>
+          <View style={styles.recipientRow}>
             <Text style={{fontSize: 20, fontWeight: 'bold'}}>Send</Text>
-            <Text style={{fontSize: 20, fontWeight: 'bold', paddingLeft: 40}}>{`${params.user.first_name} ${params.user.last_name}`}</Text>
+            <Text style={{fontSize: 20, fontWeight: 'bold', paddingLeft: 40}}>
+              {`${params.user.first_name} ${params.user.last_name}`}
+            </Text>
           </View>
-          <View style={{flexShrink: 1, justifyContent: 'space-between', flexDirection: 'row', paddingHorizontal: 20}}>
+          <View style={styles.designRow}>
             <View style={{flexShrink: 1, paddingVertical: 14}}>
-              <Text style={{flexShrink: 1, fontWeight: 'bold', flexWrap: 'wrap', fontSize: 22, paddingRight: 20}}>{design.title}</Text>
+              <Text style={styles.designTitle}>{design.title}</Text>
               <Text style={{color: 'gray'}}>Available: {formatted}</Text>
             </View>
-            <SimpleCurrencyInput onChangeText={this.onChange.bind(this)} error={error} />
+            <SimpleCurrencyInput
+              onChangeText={this.onChange.bind(this)}
+              error={error} />
           </View>
           <View style={{paddingHorizontal: 20}}>
-            <Text style={{fontSize: 16}}>Message</Text>
-            <View>
-              <TextInput
-                multiline
-                placeholder="Optional"
-                maxLength={400}
-                returnKeyType='done'
-                blurOnSubmit={true}
-                style={{borderWidth: 0.5, borderColor: 'darkgray', paddingBottom: 20}}
-                underlineColorAndroid={'transparent'}
-                onChangeText={(text) => this.setState({message: text})}
-                value={this.state.message} />
-            </View>
+            <Text style={styles.messageTitle}>
+              Add a message
+            </Text>
+            <TouchableWithoutFeedback
+              onPress={() => this.messageInput.focus()}>
+              <View style={{borderWidth: 0.5, padding: 6, minHeight: 100}}>
+                <TextInput
+                  ref={ref => (this.messageInput = ref)}
+                  multiline
+                  maxLength={this.maxMessageLength}
+                  returnKeyType='done'
+                  blurOnSubmit={true}
+                  underlineColorAndroid={'transparent'}
+                  onChangeText={(text) => this.setState({message: text})}
+                  value={message} />
+              </View>
+            </TouchableWithoutFeedback>
+            {message.length > this.maxMessageLength - 50 ? counter : null}
           </View>
           {submitting ? <Spinner /> : null}
         </View>
@@ -119,6 +145,32 @@ export class Give extends React.Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  recipientRow: {
+    flexDirection: 'row',
+    height: 60,
+    paddingHorizontal: 20,
+    borderBottomWidth: 0.5,
+    borderColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  designRow: {
+    flexShrink: 1,
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    justifyContent: 'space-between'
+  },
+  designTitle: {
+    flexShrink: 1,
+    flexWrap: 'wrap',
+    paddingRight: 20,
+    fontWeight: 'bold',
+    fontSize: 22
+  },
+  messageTitle: {fontSize: 14, color: Theme.lightBlue, marginBottom: 8}
+})
 
 Give.propTypes = {
   amounts: PropTypes.array,
