2718cf40145d0e9c56a8c7694516e6137d809799
Hunter Bowcutt <jhunterb789@gmail.com>
2019-08-09 15:25:58 -0600

    Create store avatar

---
+++ src/ferly/components/StoreAvatar.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import {Text, View, StyleSheet} from 'react-native'
+
+export default class StoreAvatar extends React.Component {
+  render () {
+    const {title, size, shade} = this.props
+    const sizeStyle = {
+      height: size,
+      width: size,
+      borderRadius: size / 2
+    }
+
+    if (title) {
+      return (
+        <View style={[styles.container, sizeStyle, shade && styles.shade]}>
+          <Text style={{fontSize: 36, color: 'gray'}}>
+            {title.charAt(0)}
+          </Text>
+        </View>
+      )
+    }
+    return null
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'lightgray',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  shade: {
+    elevation: 4,
+    shadowOffset: {width: 0, height: 2},
+    shadowColor: 'lightgray',
+    shadowOpacity: 1
+  }
+})
+
+StoreAvatar.propTypes = {
+  title: PropTypes.string,
+  shade: PropTypes.bool,
+  size: PropTypes.number.isRequired
+}
