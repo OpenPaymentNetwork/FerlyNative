22bf8426f45903271e4b597ec55671bf9ec2e330
Hunter Bowcutt <jhunterb789@gmail.com>
2020-04-28 09:43:06 -0600

    Upgrade sdk to 35

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import AppEntry from 'ferly/views/AppEntry';
 import configureStore from 'ferly/store/store.js';
 import EventListener from 'ferly/components/EventListener';
 import React from 'react';
-import Sentry from 'sentry-expo';
+import * as Sentry from 'sentry-expo';
 import {Provider} from 'react-redux';
 
-Sentry.config(
-  'https://20f3964a92714c59a5ae17e74e40c8ec@sentry.io/1398868').install();
+Sentry.init({
+  dns: 'https://20f3964a92714c59a5ae17e74e40c8ec@sentry.io/1398868',
+  enableNative: false
+});
 
 const store = configureStore();
 
